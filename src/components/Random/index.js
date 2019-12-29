cb4ff7444482d0a7b760f95c8778378e9c5f6cfb
hassen <hasansaid51@gmail.com>
2019-12-28 20:46:01 -0800

    tomorrow gotta work on cards

--- src/components/Random/index.js
+++ src/components/Random/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 export default function Random(props) {
-  const test = props.animals && props.animals[0]
-    ? props.animals[0].photos.map((elem, i) => {
-      return (
-        <ul key={i}>
-          <li>
-            <img src={elem.small} />
-          </li>
-        </ul>
-      )
-    })
-    : ""
+  // const test = props.animals && props.animals[0]
+  //   ? props.animals[0].photos.map((elem, i) => {
+  //     return (
+  //       <ul key={i}>
+  //         <li>
+  //           <img src={elem.small} />
+  //         </li>
+  //       </ul>
+  //     )
+  //   })
+  //   : ""
 
   const randomAnimals = props.animals.map((elem, i) => {
     return (
@@ -28,14 +28,12 @@ export default function Random(props) {
   })
   return (
     <div>
-      did this work?
-      <h1>Testing Photos</h1>
-      <div>{test}</div>
+      {/* <h1>Testing Photos</h1> */}
+      {/* <div>{test}</div> */}
       <hr />
       <hr />
       <hr />
       {randomAnimals}
-      {/* {test} */}
     </div>
   )
 }
