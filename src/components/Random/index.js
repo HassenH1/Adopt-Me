63e812728d6d75bc89e4c25d5fe2a728400e6360
hassen <hasansaid51@gmail.com>
2019-12-28 20:25:48 -0800

    got the api images working properly

--- src/components/Random/index.js
+++ src/components/Random/index.js
@@ -1,18 +1,41 @@
 import React from 'react'
 
 export default function Random(props) {
-  console.log(props.animals)
+  const test = props.animals && props.animals[0]
+    ? props.animals[0].photos.map((elem, i) => {
+      return (
+        <ul key={i}>
+          <li>
+            <img src={elem.small} />
+          </li>
+        </ul>
+      )
+    })
+    : ""
+
   const randomAnimals = props.animals.map((elem, i) => {
-    return(
-      <ul key={i}>
-        <li></li>
-      </ul>
+    return (
+      <div key={i}>
+        <div>
+          {elem.photos.map((el, i) => {
+            return (
+              <img key={i} src={el.small} />
+            )
+          })}
+        </div>
+      </div>
     )
   })
   return (
     <div>
       did this work?
+      <h1>Testing Photos</h1>
+      <div>{test}</div>
+      <hr />
+      <hr />
+      <hr />
       {randomAnimals}
+      {/* {test} */}
     </div>
   )
 }
