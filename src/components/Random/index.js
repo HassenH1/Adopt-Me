b7708abae68a85c05aba4e05e47d02f53f5c9525
hassen <hasansaid51@gmail.com>
2019-12-30 12:04:29 -0800

    just an error...like usual

--- src/components/Random/index.js
+++ src/components/Random/index.js
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
-// import { render } from 'react-dom';
-
+import React, { useState, useEffect } from 'react';
 import Swipeable from 'react-swipy';
-
 import Card from '../Card';
 import Button from '../Button';
 
@@ -29,43 +26,62 @@ const actionsStyles = {
 };
 
 const Random = () => {
-  // state = {
-  //   cards: ['First', 'Second', 'Third'],
-  // };
-  const [animals, setAnimals] = useState(["Hassen", "work", "please", "Im", "tired", "of", "Life"])
+  const [data, setData] = useState('')
+
+  const fetchingData = async () => {
+      const data = await fetch("http://localhost:8000/", {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+      const dataJson = await data.json()
+      console.log(dataJson, "dude wtf")
+      setData({dataJson})
+  }
+
+  useEffect(() => {
+    fetchingData()
+  }, [])
 
   const remove = () =>
-    setAnimals(( animals ) => (
-      animals.slice(1, animals.length)
+    setData((data) => (
+      data.slice(1, data.length)
     ));
 
-    return (
-      <div style={appStyles}>
-        <div style={wrapperStyles}>
-          {animals.length > 0 && (
-            <div style={wrapperStyles}>
-              <Swipeable
-                buttons={({ right, left }) => (
-                  <div style={actionsStyles}>
-                    <Button onClick={left}>Reject</Button>
-                    <Button onClick={right}>Accept</Button>
-                  </div>
-                )}
-                onAfterSwipe={remove}
-              >
-                <Card>{animals[0]}</Card>
-              </Swipeable>
-              {animals.length > 1 && (
-                <Card zIndex={-1}>{animals[1]}</Card>
+  return (
+    <div style={appStyles}>
+      {console.log(data.dataJson && data.dataJson.animals.length, "<--------")}
+      <div style={wrapperStyles}>
+        {data.dataJson && data.dataJson.animals.length > 0 && (
+          <div style={wrapperStyles}>
+            <Swipeable
+              buttons={({ right, left }) => (
+                <div style={actionsStyles}>
+                  <Button onClick={left}>Reject</Button>
+                  <Button onClick={right}>Accept</Button>
+                </div>
               )}
-            </div>
-          )}
-          {animals.length <= 1 && (
-            <Card zIndex={-2}>No more cards</Card>
-          )}
-        </div>
+              onAfterSwipe={remove}
+            >
+              <Card>{data.dataJson.animals[0]}</Card>
+              {console.log(data.dataJson.animals[0], "<----is there something here?")}
+            </Swipeable>
+            {data.dataJson && data.dataJson.animals.length > 1 && (
+              <Card zIndex={-1}>{data.dataJson.animals[1]}</Card>,
+              console.log("does it even get here bro?")
+            )}
+          </div>
+        )}
+        {console.log("what about here?")}
+        {data.dataJson && data.dataJson.animals.length <= 1 && (
+          console.log("and here?"),
+          <Card zIndex={-2}>No more cards</Card>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 export default Random;
