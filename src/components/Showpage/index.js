a790cf4222f28881a1759ee43762b291a4af569f
hassen <hasansaid51@gmail.com>
2020-01-03 17:32:17 -0800

    took out Card cuz it doesnt work

--- src/components/Showpage/index.js
+++ src/components/Showpage/index.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
-import Card from "../Card";
 import Slider from "react-slick";
 
 const wrapperStyles = {
   position: "relative",
   width: "250px",
-  height: "250px",
-  left: "32rem",
-  top: "10rem"
+  height: "321px",
+  top: "10rem",
+  margin: "0 auto",
+  textAlign: "center"
 };
 
 const settings = {
   dots: true,
-  infinite: true,
-  speed: 500,
   slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  fade: true,
 };
 
 const Image = {
@@ -43,8 +42,8 @@ export default function Showpage(props) {
 
   const images = elem.animal && elem.animal.photos.map((e, i) => {
     return (
-      <div key={i}>
-        <img src={e.full} style={Image} alt="animal"/>
+      <div key={i} style={{ width: "100vw", height: "100vh" }}>
+        <img src={e.full} style={Image} alt="animal" />
       </div>
     )
   })
@@ -52,12 +51,10 @@ export default function Showpage(props) {
   return (
     <div style={{ padding: "0", margin: "0", height: "100vh", width: "100vw" }}>
       <div style={wrapperStyles}>
-        <Card>
-          <h3>{elem.animal && elem.animal.name}</h3>
-        </Card>
         <Slider {...settings}>
           {images}
         </Slider>
+        <h3>{elem.animal && elem.animal.name}</h3>
       </div>
     </div>
   )
